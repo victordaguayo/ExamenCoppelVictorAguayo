4f82dda0584e208b0da51617820e1e241b89f7be
Victor Aguayo <victordaguayo@gmail.com>
2022-09-30 13:46:39 -0500

    commit6

---
+++ CoppelExam/MoviesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MoviesViewController.swift
+//  CoppelExam
+//
+//  Created by Victor Aguayo on 26/09/22.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController {
+
+    var recibirToken: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
